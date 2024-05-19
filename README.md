e111fb77254c66c9225f42d54a9fb1495db0581e
Pohan <oange6214@gmail.com>
2024-05-19 18:28:34 +0800

    Init ApiVersioningApp project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# [WebAPI] .NET 8 - ASP.NET Core Web API Versioning
+
+探討如何在我們的 Web API 中實施 API 版本控。 將介紹如何在程式碼中加入版本控功能，如何將其新增到 Controller 中，如何針對多個 Controller 甚至個別 Action 實施版本控，接著我們將瞭解如何透過添加一些標籤輕鬆複製特定的 API 版本。
---
+++ src/ApiVersioningApp/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc.Versioning;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApiVersioning(options => 
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new QueryStringApiVersionReader("api-version"),
+        new HeaderApiVersionReader("x-version"),
+        new MediaTypeApiVersionReader("ver")
+    );
+});
+
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
