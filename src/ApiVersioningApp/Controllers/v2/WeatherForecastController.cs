e98bdaa74f84accd13e7e21c50d5a224ab02d2f4
Pohan <oange6214@gmail.com>
2024-05-19 20:44:45 +0800

    feat: 在 Controller 中如何對 Action 設定指定版本？

--- src/ApiVersioningApp/Controllers/v2/WeatherForecastController.cs
+++ src/ApiVersioningApp/Controllers/v2/WeatherForecastController.cs
@@ -5,6 +5,7 @@ namespace ApiVersioningApp.Controllers.v2;
 [ApiController]
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiVersion("2.0")]
+[ApiVersion("2.1")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -20,13 +21,27 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
+    [MapToApiVersion("2.0")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = "The weather is nice and cool as a summer breeze"
+            Summary = "The weather is nice and cool as a summer breeze - V2"
+        })
+        .ToArray();
+    }
+
+    [HttpGet(Name = "GetWeatherForecastV21")]
+    [MapToApiVersion("2.1")]
+    public IEnumerable<WeatherForecast> GetV21()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = "The weather is hot in the summer breeze - V2.1"
         })
         .ToArray();
     }
